6eeb7b940a23d43238a46696541c4e5acfe71cd4
EternalAnime <eternalmelodymagic@gmail.com>
2024-10-26 19:47:03 +0530

    Update page.js

--- src/app/warnings/nsfw/page.js
+++ src/app/warnings/nsfw/page.js
@@ -7,17 +7,19 @@ function NSFWPage({ statusCode, reset }) {
 
     const navigations = [
       {
-       icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+       icon: <svg xmlns="https://raw.githubusercontent.com/EternalAnime/skyv1anime/main/public/SKYANIME.png
+" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
        <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
      </svg>     
      ,
        title: "Learn more why",
        desc: "Why did we remove NSFW Content?",
-       href:  "https://1anime.tawk.help/article/nsfw-policy"
+       href:  "/"
       },
         {
             icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="https://raw.githubusercontent.com/EternalAnime/skyv1anime/main/public/SKYANIME.png
+" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
                 </svg>,
             title: "Go Explore Anime",
@@ -26,12 +28,13 @@ function NSFWPage({ statusCode, reset }) {
         },
         {
             icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="https://raw.githubusercontent.com/EternalAnime/skyv1anime/main/public/SKYANIME.png
+" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                 </svg>,
             title: "Support",
             desc: "Contact Support or let us know about this error",
-            href: "https://1anime.tawk.help"
+            href: "/"
         }
     ]
 
