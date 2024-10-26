b86c93b1a2c12072ee2426fa8a863962442ccb9c
EternalAnime <eternalmelodymagic@gmail.com>
2024-10-26 19:39:43 +0530

    Update page.js

--- src/app/anime/catalog/[[...options]]/page.js
+++ src/app/anime/catalog/[[...options]]/page.js
@@ -4,13 +4,13 @@ import Navbarcomponent from '@/components/navbar/Navbar'
 
 export async function generateMetadata({ params }) {
   return {
-    title: "1Anime - Explore",
+    title: "SkyAnime - Explore",
     openGraph: {
-      title: "1Anime - Explore",
+      title: "SkyAnime - Explore",
     },
     twitter: {
       card: "summary",
-      title: "1Anime - Explore",
+      title: "SkyAnime - Explore",
     },
   }
 }
